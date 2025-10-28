3140ef6d912feb31c7b7d92825a43a6c609c9015
muhammetasari <muhammetasari@gmail.com>
2025-10-28 11:31:44 +0300

    Ali

---
+++ src/data/pois.data.ts
@@ -0,0 +1,173 @@
+export interface PoiData {
+  placeId: string;
+  name: string;
+  address: string;
+}
+
+export const POIS: PoiData[] = [
+  {
+    placeId: 'ChIJJxwBkr65yhQRrk9EN29vbiM',
+    name: 'Hagia Sophia Grand Mosque',
+    address: 'Sultan Ahmet, Ayasofya Meydanı No:1, 34122 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJM_ilr7i5yhQRAFMBw0MZqhU',
+    name: 'Topkapi Palace Museum',
+    address: 'Cankurtaran, Babı Hümayun Cad No: 1, 34122 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJ4fRwZb25yhQRpHwVijb3LeU',
+    name: 'The Blue Mosque',
+    address: 'Cankurtaran, At Meydanı Cd No:7, 34122 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJyWrG4L25yhQRcYRt7uEvwGA',
+    name: 'Basilica Cistern',
+    address: 'Alemdar, Yerebatan Cd. 1/3, 34110 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJJwXXYpG5yhQRq5jBWgtoyGQ',
+    name: 'Grand Bazaar',
+    address: 'Beyazıt, 34126 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJn9t8b-u5yhQRXVCwl43vu0Q',
+    name: 'Egyptian Bazaar',
+    address: 'Rüstem Paşa, 34116 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJMRDAOo25yhQR7c9XIQT1zIo',
+    name: 'Suleymaniye Mosque',
+    address: 'Süleymaniye, Prof. Sıddık Sami Onar Cd. No:1, 34116 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJ2XlLfO25yhQR8Hcxg0Af5V4',
+    name: 'Sultanahmet Square',
+    address: 'Binbirdirek, Sultan Ahmet Parkı No:2, 34122 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJnbx_x5e5yhQRlPF66tEpPfs',
+    name: 'Turkish & Islamic Arts Museum',
+    address: 'Binbirdirek, At Meydanı Cd No:12, 34122 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJK9q8Nce5yhQRy0am325qXgY',
+    name: 'Gülhane Park',
+    address: 'Cankurtaran, Kennedy Cad., 34122 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJQ3x3p-e5yhQRCzOLzD3Pdkw',
+    name: 'Galata Tower',
+    address: 'Bereketzade, 34421 Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJ93xst2G3yhQRAfesfO52mtU',
+    name: 'İstiklal Caddesi',
+    address: 'İstiklal Cd., Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJY71WBmW3yhQRw7YgjLJYoIw',
+    name: 'Taksim Square',
+    address: 'Taksim Square, Kocatepe, 34435 Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJLTINFuC5yhQRyRbXfXliR9k',
+    name: 'Pera Museum',
+    address: 'Asmalı Mescit, Meşrutiyet Cd. No:65, 34430 Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJG4dgweu5yhQRoGk4YDz4AJU',
+    name: 'Galata Bridge',
+    address: 'Kemankeş Karamustafa Paşa, Galata Köprüsü, 34425 Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJ4307Gna3yhQRC4M7zzg-09w',
+    name: 'Dolmabahçe Palace',
+    address: 'Vişnezade, Dolmabahçe Cd., 34357 Beşiktaş/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJrYco5Mi3yhQRvMivnqLyc_0',
+    name: 'Büyük Mecidiye Mosque',
+    address: 'Mecidiye, İskele Sk. No: 6, 34347 Beşiktaş/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJpzC--7u3yhQRXag5_ctIINI',
+    name: 'Ciragan Palace Kempinski',
+    address: 'Yıldız, Çırağan Cd. No:32, 34349 Beşiktaş/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJX0SZ6iu4yhQRbEe0zrr0124',
+    name: 'Maiden\'s Tower',
+    address: 'Salacak, 34668 Üsküdar/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJM_wBSqXJyhQRx17gdpw5I88',
+    name: 'Camlica Hill',
+    address: 'Kısıklı, Çamlıca Tepesi yl No:25/10, 34692 Üsküdar/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJs4egvY63yhQRoA-RFly9HUY',
+    name: 'Mihrimah Sultan Mosque',
+    address: 'Mimar Sinan, 34664 Üsküdar/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJXdfj1Iq4yhQR2VXUl-HxW7U',
+    name: 'Moda',
+    address: 'Moda, Caferağa, 34710 Kadıköy/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJj2j8EGy4yhQRYmTrvOcUslU',
+    name: 'Tuesday and Friday bazaar',
+    address: 'Hasanpaşa, Uzunçayır Cd., 34722 Kadıköy/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJLa5mGZa5yhQRe5Yr-Fwd-qE',
+    name: 'Bosphorus Cruise Tours Istanbul',
+    address: 'Cankurtaran, Keresteci Hakkı Sk. No:20/A, 34122 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJ-X6_lwLKyhQR-sYaKaSqV0Y',
+    name: 'Rumeli Fortress',
+    address: 'Rumeli Hisarı, Yahya Kemal Cd. No: 42, 34470 Sarıyer/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJj0QFzx_KyhQRCOw2RZg-ugo',
+    name: 'Anadoluhisarı',
+    address: 'Anadolu Hisarı, 34810 Beykoz/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJ2cMuRRC6yhQRDNb0n0-3SXA',
+    name: 'Kariye Mosque',
+    address: 'Derviş Ali, Kariye Cami Sk. No:18, 34087 Fatih/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJcSSWkbywyhQRJLkb04VnrPM',
+    name: 'Pierre Loti',
+    address: 'Merkez, İdris Köşkü Cd., 34050 Eyüpsultan/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJ9WsAScqwyhQRHNfCzoxQiTE',
+    name: 'Miniaturk',
+    address: 'Örnektepe, İmrahor Cd. No:7, 34445 Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJw8JuD9m5yhQRnegQbG9Irjs',
+    name: 'Istanbul Museum of Modern Art',
+    address: 'Kılıçali Paşa, Tophane İskele Cd. No:1/1, 34433 Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJwdTlr6uwyhQRXrwLsScTNkQ',
+    name: 'Rahmi M. Koç Museum',
+    address: 'Piri Paşa, Rahmi M. Koç Caddesi No: 3, 34445 Beyoğlu/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJC3TNb4PByhQRaCnXxJF8tVM',
+    name: 'Adalar',
+    address: 'Adalar/İstanbul, Türkiye'
+  },
+  {
+    placeId: 'ChIJVbhmQjWiyhQRlaTMLJMlTkw',
+    name: 'Florya Güneş Beach',
+    address: 'Florya Güneș Plajı, Basınköy, Florya Sahil Parkı, 34153 Bakırköy/İstanbul, Türkiye'
+  }
+];
